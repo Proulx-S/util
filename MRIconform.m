4bd87f0f018e81f7cbbc025122c49dfdbf6259c6
Sebastien Proulx <proulxs@stanford.edu>
2025-04-22 18:58:52 -0700

    don't conform if not needed

--- MRIconform.m
+++ MRIconform.m
@@ -1,12 +1,18 @@
 function MRIconform(fList, fBase)
-
+global src
     if iscell(fList)
         for f = 1:length(fList)
             MRIconform(fList{f}, fBase);
         end
         return;
     end
-    mri    = MRIread(fBase,1);
-    mriTmp = MRIread(fList  );
-    mri.vol = mriTmp.vol; clear mriTmp;
-    MRIwrite(mri,fList);
+
+    cmd = {src.afni};
+    cmd{end+1} = ['3dinfo -same_grid ' fList ' ' fBase];
+    [~,cmdout] = system(strjoin(cmd,newline));
+    if ~all(str2num(cmdout))
+        mri    = MRIread(fBase,1);
+        mriTmp = MRIread(fList  );
+        mri.vol = mriTmp.vol; clear mriTmp;
+        MRIwrite(mri,fList);
+    end
