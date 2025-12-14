46c3ae5a4146ef4b8480a88e1edbabc8b5e8497b
Sebastien Proulx <proulxs@stanford.edu>
2025-12-14 00:37:30 -0800

    wrap git clone with the option of cloning a specific subfolder

---
+++ gitClone.m
@@ -0,0 +1,22 @@
+function gitClone(url, folder, repoSubDir)
+    if ~exist('repoSubDir', 'var'); repoSubDir = []; end
+
+    if exist(fullfile(folder,repoSubDir), 'dir')
+        disp([newline url ' ' repoSubDir newline 'already downloaded to:' newline ' ' folder newline]);
+    else
+        cmd = {};
+        if isempty(repoSubDir)
+            cmd{end+1} = ['git clone ' url ' ' folder];
+        else
+            cmd{end+1} = ['git clone --filter=blob:none --sparse ' url ' ' folder];
+            cmd{end+1} = ['cd ' folder];
+            cmd{end+1} = ['git sparse-checkout init --no-cone'];
+            cmd{end+1} = ['git sparse-checkout set ' repoSubDir '/** /README* /LICENSE*'];
+            cmd{end+1} = ['git checkout'];
+        end
+        disp(strjoin(cmd,newline));
+        system(strjoin(cmd,newline));
+    end
+    addpath(genpath(fullfile(folder,repoSubDir)));
+    disp(['added to path:' newline ' ' fullfile(folder,repoSubDir)]);
+    
