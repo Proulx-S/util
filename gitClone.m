0934aa61c03542833a59ecd533ddfa23d110f6e2
Sébastien Proulx <proulxs@stanford.edu>
2026-01-14 18:13:32 -0800

    bug fix

--- gitClone.m
+++ gitClone.m
@@ -8,18 +8,17 @@ function gitClone(url, folder, repoSubDir)
         gitCheck(folder);
         
     else
-        cmd = {};
         if isempty(repoSubDir)
-            cmd{end+1} = ['git clone ' url ' ' folder];
+            cmd = ['git clone ' url ' ' folder];
+            disp(cmd);
+            system(['bash -c ''' cmd '''']);
         else
-            cmd{end+1} = ['git clone --filter=blob:none --sparse ' url ' ' folder];
-            cmd{end+1} = ['cd ' folder];
-            cmd{end+1} = ['git sparse-checkout init --no-cone'];
-            cmd{end+1} = ['git sparse-checkout set ' repoSubDir '/** /README* /LICENSE*'];
-            cmd{end+1} = ['git checkout'];
+            cmd1 = ['git clone --filter=blob:none --sparse ' url ' ' folder];
+            cmd2 = ['cd ' folder ' && git sparse-checkout init --no-cone && git sparse-checkout set ' repoSubDir '/** /README* /LICENSE* && git checkout'];
+            disp([cmd1 newline cmd2]);
+            system(['bash -c ''' cmd1 '''']);
+            system(['bash -c ''' cmd2 '''']);
         end
-        disp(strjoin(cmd,newline));
-        system(strjoin(cmd,newline));
     end
     addpath(genpath(fullfile(folder,repoSubDir)));
     disp(['added to path:' newline ' ' fullfile(folder,repoSubDir)]);
