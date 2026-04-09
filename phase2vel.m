c78545be243bde3c0ec5fee6394ec948be56b4d2
Sébastien Proulx <proulxs@stanford.edu>
2026-04-09 14:26:36 -0700

    enforce venc in dimension 5

--- phase2vel.m
+++ phase2vel.m
@@ -1,8 +1,15 @@
 function v = phase2vel(phase, venc)
     % phase: velocity encoded phase [rad]
-    % venc : velocity encoding values [cm/s]
+    % venc : velocity encoding values [cm/s]  (venc dimension is forced to the 5th)
     % v    : spin velocities [cm/s]
-    if numel(venc)~=1 && any(size(venc)~=size(phase)); dbstack; error('venc must be a scalar or the same length as phase'); end
+
+    if numel(venc)~=1
+        vencDim = size(venc,1:16)>1;
+        if nnz(vencDim)>1; dbstack; error('xx'); end
+        permIdx = circshift(1:16,5-find(vencDim));
+        venc = permute(venc, permIdx);
+    end
+    % if numel(venc)~=1 && any(size(venc)~=size(phase)); dbstack; error('venc must be a scalar or the same length as phase'); end
         
     gamma = 42.577e6;  % Gyromagnetic ratio for 1H [Hz/T]
     M1    = pi ./ (gamma .* venc./ 100);  % first moment of velocity encoding gradient [T*s^2/m]
--- vel2phase.m
+++ vel2phase.m
@@ -1,9 +1,12 @@
 function phase = vel2phase(v, venc)
     % v    : [cm/s] spin velocities 
-    % venc : [cm/s] velocity encoding values
+    % venc : [cm/s] velocity encoding values (venc dimension is forced to the 5th)
     % phase: [rad] velocity encoded phase dim [v1 v2 v3 v4 v5 v6 venc1 venc2 venc3 venc4 venc5 venc6 venc7 venc8 venc9 venc10]
     if numel(venc)~=1
-        venc = permute(venc, [13 14 15 16 1 2 3 4 5 6 7 8 9 10 11 12]);
+        vencDim = size(venc,1:16)>1;
+        if nnz(vencDim)>1; dbstack; error('xx'); end
+        permIdx = circshift(1:16,5-find(vencDim));
+        venc = permute(venc, permIdx);
     end
         
     gamma = 42.577e6;  % Gyromagnetic ratio for 1H [Hz/T]
